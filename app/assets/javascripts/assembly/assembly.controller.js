933a790a77eef33968f41ed840d304491a5b22e5
Wei Cheng <michael1996623@gmail.com>
2017-07-30 14:42:23 -0700

    feat:  Drag and Drop function supported by Angular js
    Issue:
    Solution:
    Detail:

--- app/assets/javascripts/assembly/assembly.controller.js
+++ app/assets/javascripts/assembly/assembly.controller.js
@@ -1,15 +1,15 @@
 angular.module('assembly.controller', ['dndLists', 'ui.materialize'])
     .controller('assemblyController', ['$scope', function ($scope) {
 
-        $scope.models = {
+        $scope.menu = {
             selected: null,
             lists: {"A": [], "B": []}
         };
 
-        // Generate initial model
+        // Generate initial model for Material/Process Menu List
         for (var i = 1; i <= 3; ++i) {
-            $scope.models.lists.A.push({label: "Item A" + i});
-            $scope.models.lists.B.push({label: "Item B" + i});
+            $scope.menu.lists.A.push({label: "Item A" + i});
+            $scope.menu.lists.B.push({label: "Item B" + i});
         }
 
         $scope.collapsibleElements = [{
@@ -26,5 +26,122 @@ angular.module('assembly.controller', ['dndLists', 'ui.materialize'])
             content: 'Lorem ipsum dolor sit amet.'
         }
         ];
+
+        // Generate initial model for Building Blocks
+        $scope.models = {
+            selected: null,
+            templates: [
+                {type: "item", id: 2},
+                {type: "container", id: 1, columns: [[], []]}
+            ],
+            dropzones: {
+                "A": [
+                    {
+                        "type": "container",
+                        "id": 1,
+                        "columns": [
+                            [
+                                {
+                                    "type": "item",
+                                    "id": "1"
+                                },
+                                {
+                                    "type": "item",
+                                    "id": "2"
+                                }
+                            ],
+                            [
+                                {
+                                    "type": "item",
+                                    "id": "3"
+                                }
+                            ]
+                        ]
+                    },
+                    {
+                        "type": "item",
+                        "id": "4"
+                    },
+                    {
+                        "type": "item",
+                        "id": "5"
+                    },
+                    {
+                        "type": "item",
+                        "id": "6"
+                    }
+                ],
+                "B": [
+                    {
+                        "type": "item",
+                        "id": 7
+                    },
+                    {
+                        "type": "item",
+                        "id": "8"
+                    },
+                    {
+                        "type": "container",
+                        "id": "2",
+                        "columns": [
+                            [
+                                {
+                                    "type": "item",
+                                    "id": "9"
+                                },
+                                {
+                                    "type": "item",
+                                    "id": "10"
+                                },
+                                {
+                                    "type": "item",
+                                    "id": "11"
+                                }
+                            ],
+                            [
+                                {
+                                    "type": "item",
+                                    "id": "12"
+                                },
+                                {
+                                    "type": "container",
+                                    "id": "3",
+                                    "columns": [
+                                        [
+                                            {
+                                                "type": "item",
+                                                "id": "13"
+                                            }
+                                        ],
+                                        [
+                                            {
+                                                "type": "item",
+                                                "id": "14"
+                                            }
+                                        ]
+                                    ]
+                                },
+                                {
+                                    "type": "item",
+                                    "id": "15"
+                                },
+                                {
+                                    "type": "item",
+                                    "id": "16"
+                                }
+                            ]
+                        ]
+                    },
+                    {
+                        "type": "item",
+                        "id": 16
+                    }
+                ]
+            }
+        };
+
+        $scope.$watch('models.dropzones', function(model) {
+            $scope.modelAsJson = angular.toJson(model, true);
+        }, true);
 
     }]);
