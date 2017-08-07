05613987718bb7dadc7958d8ad93aaa62fd2fdbf
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-08-06 21:52:03 -0700

    maint: Maintenance tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,7 @@
 # newer version of cucumber-rails. Consider adding your own code to a new file
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
-require "simplecov"
-SimpleCov.start
+
 require 'cucumber/rails'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
@@ -57,4 +56,3 @@ end
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
-World(FactoryGirl::Syntax::Methods)
