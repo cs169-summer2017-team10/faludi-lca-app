f7aa4c0059178cffa75b094374d91a0696de933d
Mehdi Kharraz <kharraz.mehdi@gmail.com>
2017-08-10 07:04:13 -0700

    fix: Fix heroku

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -15,21 +15,21 @@ function routesConfig($stateProvider, $urlRouterProvider){
             url: '/login',
             templateUrl: 'views/login.html',
             controller: 'AuthCtrl',
-            onEnter: function(Auth, $state){
-                Auth.currentUser().then(function(){
-                    $state.go('home')
+            onEnter: ['Auth', '$state', function(Auth, $state) {
+                Auth.currentUser().then(function() {
+                    $state.go('home');
                 })
-            }
+            }]
         })
         .state('register', {
             url: '/register',
             templateUrl: 'views/register.html',
             controller: 'AuthCtrl',
-            onEnter: function(Auth, $state){
-                Auth.currentUser().then(function(){
-                    $state.go('home')
+            onEnter: ['Auth', '$state', function(Auth, $state) {
+                Auth.currentUser().then(function() {
+                    $state.go('home');
                 })
-            }
+            }]
         })
         .state('assembly-new', {
             url: '/assembly',
